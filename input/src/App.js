1b44a98a99fb327421b3a719c2829e7fe2b7cebd
Mootbing <50122069+Mootbing@users.noreply.github.com>
2022-11-08 18:58:53 -0500

    works

---
+++ input/src/App.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+import { useQuill } from 'react-quilljs';
+// or const { useQuill } = require('react-quilljs');
+
+import 'quill/dist/quill.snow.css'; // Add css for snow theme
+// or import 'quill/dist/quill.bubble.css'; // Add css for bubble theme
+
+var toolbarOptions = [
+  [{ font: [] }],
+  [{ size: ['small', false, 'large', 'huge'] }],
+
+  [],
+
+  ["bold", "italic", "underline", "strike"], 
+
+  [],
+
+  [{ color: [] }, { background: [] }],
+
+  [],
+
+  [{ header: 1 }, { header: 2 }, "blockquote", "code-block"],
+
+  [],
+
+  [{ list: "ordered" }, { list: "bullet" }, { indent: "-1" }, { indent: "+1" }],
+
+  [],
+
+  [{ script: "sub" }, { script: "super" }], // superscript/subscript
+
+  [],
+
+  [{ align: [] }, { direction: "rtl" }], // text direction
+
+  [],
+
+  ['link', 'image', 'video'],
+
+  [],[],[],
+
+  ["clean"], // remove formatting button
+];
+
+export default function App() {
+  const { quillRef } = useQuill({theme: "snow", modules: {toolbar: toolbarOptions}, formats: [
+    'bold', 'italic', 'underline', 'strike',
+    'align', 'list', 'indent',
+    'size', 'header',
+    'link', 'image', 'video',
+    'color', 'background',
+    'clean',
+  ], placeholder: ""});
+
+  return (
+    <div style={{ width: "100%", height: "100%" }}>
+      <div ref={quillRef} />
+    </div>
+  );
+};
